a67b9c65722fd90de6730bfaa6b8064e0c7cfbd0
jakmat <jakubmatusiak87@gmail.com>
2020-09-28 21:07:49 +0200

    * [refactored] Update observer pattern

--- src/scripts/observer.pattern.js
+++ src/scripts/observer.pattern.js
@@ -1,70 +1,91 @@
 import { setElement } from "./addElement";
+import { v4 as uuidv4 } from 'uuid';
 /**
  * Observer Pattern DEMO
  * ------------------------
  * Article:
  */
-const subject = () => {
-  return {};
+const subject = (display) => {
+  const instance = {
+    display,
+    number: 0,
+    observers: [],
+    attach(observer) {
+      this.observers.push(observer);
+    },
+    detach(observer) {
+      this.observers = this.observers.filter(o => o.id !== observer.id);
+    },
+    notify() {
+      this.observers.forEach(o => o.update(this.number))
+    },
+    randomize() {
+      instance.number = Math.floor(Math.random() * Math.floor(99));
+      instance.display.innerHTML = instance.number;
+      instance.notify();
+    }
+  };
+  return instance;
 };
 
-const observer = () => {
-  return {};
+const observer = (name, display, button, subjectToObserve) => {
+  const instance = {
+    id: uuidv4(),
+    name,
+    display,
+    button,
+    subjectToObserve,
+    isSubscribed: false,
+    currentNumber: null,
+    update(number) {
+      instance.currentNumber = number;
+      instance.display.innerHTML = instance.currentNumber;
+    },
+    subscribe() {
+      instance.isSubscribed = true;
+      instance.button.innerHTML = 'Unsubscribe';
+      instance.subjectToObserve.attach(this);
+    },
+    unsubscribe() {
+      instance.isSubscribed = false;
+      instance.button.innerHTML = 'Subscribe';
+      instance.subjectToObserve.detach(this);
+    },
+    toggleSubscription() {
+      if (instance.isSubscribed) instance.unsubscribe();
+      else instance.subscribe();
+    }
+  };
+  return instance;
 };
-
 
 const observerPattern = {
-  isObserverSubscribedToSubject: false,
-  subjectItem: null,
-  subjectRandomizer: null,
-  subjectSubscriber: null,
-  subjectSpan: null,
-  observer: {
-    name: 'NumberObserver',
-    isObserving: false,
-    span: null,
-    commit(event) {
-      this.span.innerHTML = event.detail.number;
-    }
+  subjectView: null,
+  subjectBtn: null,
+  subjectDisplay: null,
+  observerView: null,
+  observerBtn: null,
+  observerDisplay: null,
+  initializeView(listParentElement) {
+    this.subjectView = setElement(listParentElement, 'li');
+    this.subjectBtn = setElement(this.subjectView, 'button');
+    this.subjectDisplay = setElement(this.subjectView, 'span');
+    this.observerView = setElement(listParentElement, 'li');
+    this.observerBtn = setElement(this.observerView, 'button');
+    this.observerDisplay = setElement(this.observerView, 'span');
   },
-  onShuffle: null,
-  observerItem: null,
-  observerSubscriber: null,
-  observerSpan: null,
-  onSubscribe: null,
-  onRandomize: null,
   demo(listParentElement) {
-    this.subjectItem  = setElement(listParentElement, 'li');
-    this.subjectRandomizer = setElement(this.subjectItem, 'button');
-    this.subjectRandomizer.innerHTML = 'Shuffle number';
-    this.subjectSpan = setElement(this.subjectItem, 'span');
-    this.onShuffle = () => {
-      this.subjectSpan.innerHTML = Math.floor(Math.random() * Math.floor(99));
-      const event = new CustomEvent('randomize', { detail: {
-          number: this.subjectSpan.innerHTML
-        } });
-      if (this.subjectSubscriber) this.subjectSubscriber.commit(event);
-    };
-    this.subjectRandomizer.addEventListener('click', this.onShuffle);
-    this.subjectSpan.innerHTML = 0;
-    this.onShuffle = () => {
-
-    },
-    this.observerItem = setElement(listParentElement, 'div');
-    this.observerSubscriber = setElement(this.observerItem, 'button');
-    this.observer.span = setElement(this.observerItem, 'span');
-    this.observerSubscriber.innerHTML = 'Subscribe';
-
-    this.onSubscribe = () => {
-      this.isObserverSubscribedToSubject = !this.isObserverSubscribedToSubject;
-      this.observerSubscriber.innerHTML = this.isObserverSubscribedToSubject ? 'Unsubscribe' : 'Subscribe';
-      this.observer.isObserving = this.isObserverSubscribedToSubject;
-      this.subjectSubscriber = this.isObserverSubscribedToSubject ? this.observer : null;
-    },
-    this.observerSubscriber.addEventListener('click', this.onSubscribe);
-    this.onRandomize = (event) => {
-      this.observerSpan.innerHTML = event.detail.number;
-    };
+    this.initializeView(listParentElement);
+    // concrete subject
+    const concreteSubject = subject(this.subjectDisplay);
+    this.subjectBtn.addEventListener('click', concreteSubject.randomize);
+    this.subjectBtn.innerHTML = 'Randomize Number';
+    this.subjectDisplay.innerHTML = '--';
+    // concrete observer
+    const concreteObserver = observer('Tony', this.observerDisplay, this.observerBtn, concreteSubject);
+    this.observerBtn.addEventListener('click', concreteObserver.toggleSubscription);
+    this.observerBtn.innerHTML = 'Subscribe';
+    this.observerDisplay.innerHTML = '--';
   }
 };
 
